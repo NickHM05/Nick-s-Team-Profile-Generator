54d51214d44e446df02a75172af5efc346ab9f73
NicholasHM05 <nhmamberger@gmail.com>
2023-01-11 11:25:30 -0600

    saving cards made for engineer, intern, and manager

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,3 +4,13 @@ const Engineer = require("../lib/Engineer");
 // Extendsgithub => github username 
 // getGithub
 // getRole()=> Overridden to return 'Engineer'
+test('can instantiate engineer object', () =>
+{
+    const engineer = new Engineer('Nick', 27, 'nhmamberger@company.com', 'NickHM05')
+
+expect(engineer.name).toEqual(expect.any(String));
+expect(engineer.id).toEqual(expect.any(Number));
+expect(engineer.email).toEqual(expect.any(String));
+expect(engineer.github).toEqual(expect.any(String));
+
+})
